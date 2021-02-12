856a95423b4eb7b37c1c64f6191b6d1e5b5b587e
sabinalebediene <70894973+sabinalebediene@users.noreply.github.com>
2021-02-12 08:56:21 +0200

    New project, .env, entities

---
+++ src/Entity/Notes.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NotesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NotesRepository::class)
+ */
+class Notes
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $priority;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $status_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPriority(): ?string
+    {
+        return $this->priority;
+    }
+
+    public function setPriority(string $priority): self
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(string $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getStatusId(): ?int
+    {
+        return $this->status_id;
+    }
+
+    public function setStatusId(int $status_id): self
+    {
+        $this->status_id = $status_id;
+
+        return $this;
+    }
+}
